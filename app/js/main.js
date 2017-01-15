4b692dc17eee3baef79aeef05fdb3b4e94e942ac
Vsevolod <dramdoobas123456@gmail.com>
2017-01-16 02:13:50 +0300

    Sass Guidelines
    1.  https://sass-guidelin.es/ru/#section-48 . Изменение структуры,
    согласно этому руководству.
    2. Небольшие правки по сайту.

--- app/js/main.js
+++ app/js/main.js
@@ -1,77 +1,117 @@
 $(document).ready(function(){
 
-		// VARIABLES
-		var header_menu_name 	= 'vnav-menu',
-				header_menu 			= $('.' + header_menu_name),
-				menuActive        = 'vnav-open',
-				menuBtn           = $('.js-toggle-menu'),
-				body 							= $("body"),
-				visibility        = "in visible",
-				active            = "active",
-		 		backdrop = $("<div />", {
-					class: "vmodal-backdrop fade"
-				});
-		//MENU
-			//SCRIPTS
-			function toggler(){
-				body.toggleClass(menuActive);
-			}
-			menuBtn.click(function(){
-				var menu_href = $(this).attr("data-menu");
-				menuBtn.not($(this)).removeClass(active);
-				$(this).toggleClass(active);
-				toggler();
+	// VARIABLES
+	var header            			= $(".header"),
+			headerInner 						= $('.header-inner'),
+			headerInnerHeight 			= headerInner.outerHeight(),
+			headerInnerOffset 			= headerInner.offset().top,
+			headerRange 						= 340,
+			header_menu 						= $('.vnav-menu'),
+			menuActive        			= 'vnav-open',
+			menuBtn           			= $('.js-toggle-menu'),
+			body 										= $("body"),
+			visibility        			= "in visible",
+			active            			= "active",
+	 		backdrop 							= $("<div />", {
+				class: "vmodal-backdrop fade"
 			});
-			$('.anchor-menu, a[href*="#"]').click(function(){
-				var href = $(this).attr('href');
-				$('body,html').animate({
-					scrollTop: $(href).offset().top - 50
-				},2000);
-				$("[data-menu]").removeClass(active);
-				toggler();
-				return false;
-			});
-		//HIGHLIGHT POINT MENU
-			function highlightPoints(){
-				$(".target").each(function(){
-					if( $(window).scrollTop() >= $(this).offset().top - $('.vnav-menu a').outerHeight() ){
-						var id = $(this).attr('id');
-						$('.vnav-menu a').removeClass(active);
-						$('.vnav-menu a[href="#'+ id +'"]').addClass(active);
-					}
-				});
-			}
-		//STYLING APPEAR ELEMENTS
-			var elementVisible = function(){
-				$(".vloading-mask").each(function(){
-					if ( $(this).visible() ) {
-						$(this).addClass(active);
-					}				
-				});
-			}
-
-		//CALL FUNCTIONS
-			elementVisible();
-			highlightPoints();
-			$(window).on('scroll', function(){
-				elementVisible();
-				highlightPoints();
-				if($(this).scrollTop() >= 200){
-					menuBtn.addClass('scrolled');
-				}else{
-					menuBtn.removeClass('scrolled');
-				}
-			});
-
-		// SCROLL TO BLOCK
-		$('.anchor').click(function(){
-			var href = $(this).attr('href');
-			$('body,html').animate({
-				scrollTop: $(href).offset().top
-			},2000);
-			return false;
-		});
+	// Smooth page scrolling
 		
+	function toggler(){
+		body.toggleClass(menuActive);
+	}
+	// Клик на кнопку меню
+	menuBtn.click(function(){
+		var menu_href = $(this).attr("data-menu");
+		menuBtn.not($(this)).removeClass(active);
+		$(this).toggleClass(active);
+		toggler();
+	});
+
+	// Клики на пункты меню
+	$('.anchor-menu, a[href*="#"]').click(function(){
+		var href = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $(href).offset().top - 50
+		},2000);
+		$("[data-menu]").removeClass(active);
+		toggler();
+		return false;
+	});
+
+	// SCROLL TO BLOCK
+	$('.anchor').click(function(){
+		var href = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $(href).offset().top
+		},2000);
+		return false;
+	});
+
+	// Подсвечивание пунктов меню
+	function highlightPoints(){
+		$(".target").each(function(){
+			if( $(window).scrollTop() >= $(this).offset().top - $('.vnav-menu a').outerHeight() ){
+				var id = $(this).attr('id');
+				$('.vnav-menu a').removeClass(active);
+				$('.vnav-menu a[href="#'+ id +'"]').addClass(active);
+			}
+		});
+	}
+
+	// Добавление активности для масок
+	function maskVisible(){
+		$(".vloading-mask").each(function(){
+			if ( $(this).visible() ) {
+				$(this).addClass(active);
+			}				
+		});
+	}
+	function scrollingFunctions(){
+		maskVisible();
+		highlightPoints();
+	}
+
+	// Вызов 2 функций: highlightPoints(), maskVisible()
+		scrollingFunctions();
+
+	$(window).scroll(function(){
+
+		scrollingFunctions();
+
+		var scrollTop = $(this).scrollTop(), // Скролл сверху
+				offset = headerInnerOffset + (headerInnerHeight / 3), // Отступ сверху у контейнера внутри .header + высота этого же контейнера / 2
+				calc = 1 - (scrollTop/2 - offset + headerRange / 2) / headerRange; 
+
+		headerInner.css({ 'opacity': calc });
+		if ( calc >= '1' ) {
+      headerInner.css({ 'opacity': 1 });
+    } else if ( calc <= '0' ) {
+      headerInner.css({ 'opacity': 0 });
+    }
+		// Toggling classes for menu button
+		if(scrollTop >= 200){
+			menuBtn.addClass('scrolled');
+		}else{
+			menuBtn.removeClass('scrolled');
+		}
+		if(scrollTop <= header.outerHeight()){
+			$('.header-logo').css({
+				'transform': 'translate(0px, -'+scrollTop/2+'%)'
+			});
+			$('.header-text').css({
+				'transform': 'translate(0px, '+scrollTop/8+'%)'
+			});
+			// $('.header-name').css({
+			// 	'transform': 'translate(0px, '+scrollTop/8+'%)'
+			// });
+			// $('.header-tagline').css({
+			// 	'transform': 'translate(0px, '+scrollTop/2+'%)'
+			// });
+		}
+	});
+	
+	// Modal 
 		$('[data-modal="vmodal"]').click(function(){
 			var thisTarget = $(this).attr("data-modal-target");
 			if ( thisTarget ) {
@@ -82,11 +122,13 @@ $(document).ready(function(){
 				console.log("Need attribtue [data-modal-target].");
 			}
 		});
+		// Закрытие модального окна, при нажатии на крестик
 		$('[data-close="vmodal"]').click(function(){
 			$(this).closest(".vmodal").removeClass(visibility);
 			backdrop.removeClass(visibility);
 			body.removeClass("vmodal-open");
 		});
+		// Нажатие на пустое место, при открытом модальном окне
 		$(window).click(function(e){
 			if ( backdrop.length > 0 ) {
 				if ( $(e.target).is(".vmodal") ) {
@@ -97,47 +139,27 @@ $(document).ready(function(){
 			}
 		});
 
-		//COLLAPSE
-			$(".vcollapse-inner.active").children(".vcollapse-body").slideDown();
-			$(".vcollapse-header").click(function(){
-				$(this).parent().toggleClass(active);
-				$(this).next().slideToggle("slow");
-				$(this).closest(".vcollapse-wrap").children(".vcollapse-inner").not($(this).parent()).removeClass("active");
-				$(this).closest(".vcollapse-wrap").children(".vcollapse-inner").children(".vcollapse-body").not($(this).next()).slideUp("slow");
-			});
-		//TABS
-			$('[data-tab="tab"]').click(function(){
-				//TABS LINK TOGGLE ACTIVE CLASS
-					$(this).closest(".vtabs-list").children("li").removeClass(active);
-					$(this).parent().addClass(active);
-				//TABS CONTENT SHOW
-					var tabTarget = $(this).attr('data-tab-target');
-					$(tabTarget).addClass(active);
-					$(".vtabs-content > div").not($(tabTarget)).removeClass(active);
-					$('body,html').animate({
-						scrollTop: $(tabTarget).offset().top
-					},2000);
-			});
+	//COLLAPSE
+		$(".vcollapse-inner.active").children(".vcollapse-body").slideDown();
+		$(".vcollapse-header").click(function(){
+			$(this).parent().toggleClass(active);
+			$(this).next().slideToggle("slow");
+			$(this).closest(".vcollapse-wrap").children(".vcollapse-inner").not($(this).parent()).removeClass("active");
+			$(this).closest(".vcollapse-wrap").children(".vcollapse-inner").children(".vcollapse-body").not($(this).next()).slideUp("slow");
+		});
 
-		
-			var header = $('.header .vcontainer'),
-					height = header.outerHeight(),
-					range = 200;
-			$(window).scroll(function(){
- 				var scrollTop = $(this).scrollTop(),
- 						offset = header.offset().top;
-    		
-    		offset = offset + height / 2;
-    		var calc = 1 - (scrollTop - offset + range / 2.5) / range;
-    		header.css({ 'opacity': calc });
-    		if ( calc >= '1' ) {
-		      header.css({ 'opacity': 1 });
-		    } else if ( calc <= '0' ) {
-		      header.css({ 'opacity': 0 });
-		    }
-			});
-		//$(window).stellar();
-		//DEVELOPE
-		// var widthDevice = $(window).width();
-		// $(".development").html(widthDevice);
+	//TABS
+		$('[data-tab="tab"]').click(function(){
+			//TABS LINK TOGGLE ACTIVE CLASS
+				$(this).closest(".vtabs-list").children("li").removeClass(active);
+				$(this).parent().addClass(active);
+			//TABS CONTENT SHOW
+				var tabTarget = $(this).attr('data-tab-target');
+				$(tabTarget).addClass(active);
+				$(".vtabs-content > div").not($(tabTarget)).removeClass(active);
+				$('body,html').animate({
+					scrollTop: $(tabTarget).offset().top
+				},2000);
+		});
+
 });	
